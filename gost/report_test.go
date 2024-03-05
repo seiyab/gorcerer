d9b5bc03fabcdddf8770efc3f1daaa3b5edf58dc
seiya <20365512+seiyab@users.noreply.github.com>
2024-03-06 08:53:35 +0900

    rewrite test

--- gost/report_test.go
+++ gost/report_test.go
@@ -9,32 +9,44 @@ import (
 )
 
 func TestUnmarshalReport(t *testing.T) {
-	d := `
-{
-	"go.uber.org/zap/zapcore [go.uber.org/zap/zapcore.test]": {
-		"handleerror": [
-			{
-				"posn": "/path/to/repo/zapcore/json_encoder_impl_test.go:618:2",
-				"message": "unhandled error"
-			}
-		]
-	}
-}
-	`
-	var actual gost.Report
-	if err := json.Unmarshal([]byte(d), &actual); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, gost.Report{
-		"go.uber.org/zap/zapcore [go.uber.org/zap/zapcore.test]": {
-			"handleerror": []gost.Diagnostic{
-				{
-					File:    "/path/to/repo/zapcore/json_encoder_impl_test.go",
-					Line:    618,
-					Column:  2,
-					Message: "unhandled error",
+	testCases := []struct {
+		name     string
+		input    string
+		expected gost.Report
+	}{
+		{
+			name: "simple",
+			input: `{
+				"go.uber.org/zap/zapcore [go.uber.org/zap/zapcore.test]": {
+					"handleerror": [
+						{
+							"posn": "/path/to/repo/zapcore/json_encoder_impl_test.go:618:2",
+							"message": "unhandled error"
+						}
+					]
+				}
+			}`,
+			expected: gost.Report{
+				"go.uber.org/zap/zapcore [go.uber.org/zap/zapcore.test]": {
+					"handleerror": []gost.Diagnostic{
+						{
+							File:    "/path/to/repo/zapcore/json_encoder_impl_test.go",
+							Line:    618,
+							Column:  2,
+							Message: "unhandled error",
+						},
+					},
 				},
 			},
 		},
-	}, actual)
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var actual gost.Report
+			if err := json.Unmarshal([]byte(tc.input), &actual); err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
 }
