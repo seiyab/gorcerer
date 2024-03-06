17d4719d72b521519a883bc41b0a41793d62e186
seiya <20365512+seiyab@users.noreply.github.com>
2024-03-06 12:52:02 +0900

    add test

--- gost/report_test.go
+++ gost/report_test.go
@@ -39,6 +39,69 @@ func TestUnmarshalReport(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "multiple",
+			input: `{
+				"github.com/cli/cli/v2/pkg/cmd/extension": {
+					"openfileflag": [
+						{
+							"posn": "path/to/repo/pkg/cmd/extension/manager.go:379:12",
+							"message": "O_TRUNC / O_APPEND / O_EXCL flags are not specified"
+						}
+					]
+				},
+				"github.com/cli/cli/v2/pkg/cmd/extension [github.com/cli/cli/v2/pkg/cmd/extension.test]": {
+					"openfileflag": [
+						{
+							"posn": "path/to/repo/pkg/cmd/extension/manager.go:379:12",
+							"message": "O_TRUNC / O_APPEND / O_EXCL flags are not specified"
+						},
+						{
+							"posn": "path/to/repo/pkg/cmd/extension/manager_test.go:1161:11",
+							"message": "O_TRUNC / O_APPEND / O_EXCL flags are not specified"
+						},
+						{
+							"posn": "path/to/repo/pkg/cmd/extension/manager_test.go:1185:12",
+							"message": "O_TRUNC / O_APPEND / O_EXCL flags are not specified"
+						}
+					]
+				}
+			}`,
+			expected: gost.Report{
+				"github.com/cli/cli/v2/pkg/cmd/extension": {
+					"openfileflag": []gost.Diagnostic{
+						{
+							File:    "path/to/repo/pkg/cmd/extension/manager.go",
+							Line:    379,
+							Column:  12,
+							Message: "O_TRUNC / O_APPEND / O_EXCL flags are not specified",
+						},
+					},
+				},
+				"github.com/cli/cli/v2/pkg/cmd/extension [github.com/cli/cli/v2/pkg/cmd/extension.test]": {
+					"openfileflag": []gost.Diagnostic{
+						{
+							File:    "path/to/repo/pkg/cmd/extension/manager.go",
+							Line:    379,
+							Column:  12,
+							Message: "O_TRUNC / O_APPEND / O_EXCL flags are not specified",
+						},
+						{
+							File:    "path/to/repo/pkg/cmd/extension/manager_test.go",
+							Line:    1161,
+							Column:  11,
+							Message: "O_TRUNC / O_APPEND / O_EXCL flags are not specified",
+						},
+						{
+							File:    "path/to/repo/pkg/cmd/extension/manager_test.go",
+							Line:    1185,
+							Column:  12,
+							Message: "O_TRUNC / O_APPEND / O_EXCL flags are not specified",
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
